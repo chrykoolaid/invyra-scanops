74266679be61aaf9f32e74f050d69334e638701b
chrykoolaid <atillacokyavuz1992@gmail.com>
2026-06-25 21:50:34 +0800

    phase 5b disabled outbound candidate preview exports

---
+++ src/inventory-bridge/outboundCandidate/index.js
@@ -0,0 +1,11 @@
+export {
+  SCANOPS_BRIDGE_OUTBOUND_CANDIDATE_COMPONENT,
+  SCANOPS_BRIDGE_OUTBOUND_CANDIDATE_PHASE,
+  SCANOPS_BRIDGE_OUTBOUND_CANDIDATE_REASONS,
+  SCANOPS_BRIDGE_OUTBOUND_CANDIDATE_STATUSES,
+  buildScanOpsBridgeOutboundCandidatePreview,
+} from './outboundCandidatePreview.js';
+
+export {
+  getScanOpsBridgeOutboundCandidatePreviewDiagnostics,
+} from './outboundCandidateDiagnostics.js';
