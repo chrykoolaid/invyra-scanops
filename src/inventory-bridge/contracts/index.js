fefce1d259a9c41390bd813ee36362df1c8b20f3
chrykoolaid <atillacokyavuz1992@gmail.com>
2026-06-25 19:57:09 +0800

    phase 5a scanops disabled contract adapter

---
+++ src/inventory-bridge/contracts/index.js
@@ -0,0 +1,12 @@
+export {
+  SCANOPS_BRIDGE_CONTRACT_ADAPTER_COMPONENT,
+  SCANOPS_BRIDGE_CONTRACT_ADAPTER_PHASE,
+  SCANOPS_BRIDGE_EVENT_ENVELOPE_CLASSIFICATIONS,
+  assessScanOpsBridgeEventEnvelopeContract,
+  classifyScanOpsBridgeEventEnvelopeShape,
+  normalizeScanOpsBridgeEventEnvelopeCandidate,
+} from './eventEnvelopeContract.js';
+
+export {
+  getScanOpsBridgeEventEnvelopeContractDiagnostics,
+} from './eventEnvelopeDiagnostics.js';
