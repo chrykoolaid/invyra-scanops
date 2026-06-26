ba30c462d830c89af81ac3fbf0a4c3b4f1fede0c
chrykoolaid <atillacokyavuz1992@gmail.com>
2026-06-26 18:03:34 +0800

    feat(scanops-bridge): export Phase 7 handshake preparation

---
+++ src/inventory-bridge/trainingHandshake/index.js
@@ -0,0 +1,17 @@
+export {
+  SCANOPS_BRIDGE_TRAINING_HANDSHAKE_BLOCKERS,
+  SCANOPS_BRIDGE_TRAINING_HANDSHAKE_COMPONENT,
+  SCANOPS_BRIDGE_TRAINING_HANDSHAKE_FIXTURES,
+  SCANOPS_BRIDGE_TRAINING_HANDSHAKE_PHASE,
+  SCANOPS_BRIDGE_TRAINING_HANDSHAKE_STATUSES,
+} from './trainingHandshakeFixtures.js';
+
+export {
+  buildScanOpsBridgeTrainingHandshakeReadiness,
+  getScanOpsBridgeTrainingHandshakeReadinessResults,
+  projectScanOpsBridgeTrainingHandshakeReadinessResult,
+} from './trainingHandshakeReadiness.js';
+
+export {
+  getScanOpsBridgeTrainingHandshakeStatus,
+} from './trainingHandshakeStatus.js';
