d62117a2b5357c4083a1b9663ec033c23354770e
chrykoolaid <atillacokyavuz1992@gmail.com>
2026-06-26 17:50:05 +0800

    feat(scanops-bridge): export disabled local transport scaffold

---
+++ src/inventory-bridge/localTransport/index.js
@@ -0,0 +1,17 @@
+export {
+  SCANOPS_BRIDGE_LOCAL_TRANSPORT_BLOCKERS,
+  SCANOPS_BRIDGE_LOCAL_TRANSPORT_COMPONENT,
+  SCANOPS_BRIDGE_LOCAL_TRANSPORT_FIXTURES,
+  SCANOPS_BRIDGE_LOCAL_TRANSPORT_PHASE,
+  SCANOPS_BRIDGE_LOCAL_TRANSPORT_STATUSES,
+} from './localTransportFixtures.js';
+
+export {
+  buildScanOpsBridgeLocalTransportPreflight,
+  getScanOpsBridgeLocalTransportPreflightResults,
+  projectScanOpsBridgeLocalTransportPreflightResult,
+} from './localTransportPreflight.js';
+
+export {
+  getScanOpsBridgeLocalTransportDiagnostics,
+} from './localTransportDiagnostics.js';
