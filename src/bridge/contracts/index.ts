ed14462126f6dc0b21ebe04b4c462dde9dec757d
chrykoolaid <atillacokyavuz1992@gmail.com>
2026-07-01 21:59:54 +0800

    phase 32 cleanup: align contracts index exports

--- src/bridge/contracts/index.ts
+++ src/bridge/contracts/index.ts
@@ -19,6 +19,22 @@ export {
   createBridgeManualSyncBoundaryReport,
 } from "./bridgeManualSyncBoundaryReport";
 
+export {
+  createBridgePhase32ValidationManifest,
+} from "./bridgePhase32ValidationManifest";
+
+export {
+  createBridgePhase32ClosureSnapshot,
+} from "./bridgePhase32ClosureSnapshot";
+
+export {
+  createBridgePhase32NextGateDecision,
+} from "./bridgePhase32NextGateDecision";
+
+export {
+  createBridgePhase32CleanupReport,
+} from "./bridgePhase32CleanupReport";
+
 export type {
   BridgeContractRegistryEntry,
   BridgeContractRegistryName,
@@ -42,3 +58,37 @@ export type {
   BridgeManualSyncBoundaryReport,
   BridgeManualSyncBoundaryReportPhase,
 } from "./bridgeManualSyncBoundaryReportTypes";
+
+export type {
+  BridgeExternalBridgeSurfaceEntry,
+  BridgePhase32ValidationEntry,
+  BridgePhase32ValidationEntryStatus,
+  BridgePhase32ValidationManifest,
+  BridgePhase32ValidationManifestPhase,
+  BridgePhase32ValidationManifestTotals,
+} from "./bridgePhase32ValidationManifestTypes";
+
+export type {
+  BridgePhase32ClosureSnapshot,
+  BridgePhase32ClosureSnapshotPhase,
+  BridgePhase32ClosureSnapshotTotals,
+  BridgePhase32ClosureStatus,
+  BridgePhase32CurrentExternalBridgeSurface,
+} from "./bridgePhase32ClosureSnapshotTypes";
+
+export type {
+  BridgePhase32GateOutcome,
+  BridgePhase32MergedExternalSurface,
+  BridgePhase32NextAllowedStep,
+  BridgePhase32NextGateDecision,
+  BridgePhase32NextGateDecisionPhase,
+  BridgePhase32NextGateDecisionTotals,
+  BridgePhase32PendingExternalDraftSurface,
+} from "./bridgePhase32NextGateDecisionTypes";
+
+export type {
+  BridgePhase32CleanupReport,
+  BridgePhase32CleanupReportPhase,
+  BridgePhase32CleanupReportTotals,
+  BridgePhase32CleanupStatus,
+} from "./bridgePhase32CleanupReportTypes";
